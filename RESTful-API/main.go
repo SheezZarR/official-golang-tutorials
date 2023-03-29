487e26c3d39083b022e3e790e7052f934157e037
Iaroslav Baslin <wertout77@gmail.com>
2023-03-29 17:09:21 +0300

    feat: retrieve single album

--- RESTful-API/main.go
+++ RESTful-API/main.go
@@ -19,6 +19,7 @@ var albums = []album{
 	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
 }
 
+// getAlbums responds with the list of all albums as JSON.
 func getAlbums(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, albums)
 }
@@ -27,18 +28,33 @@ func getAlbums(c *gin.Context) {
 func postAlbums(c *gin.Context) {
 
 	var newAlbum album
-	
+
 	if err := c.BindJSON(&newAlbum); err != nil {
-		return
+		return 
 	}
 
 	albums = append(albums, newAlbum)
 	c.IndentedJSON(http.StatusCreated, newAlbum)
 }
 
+// getAlbumByID locates the album whose ID value matches the id
+// parameter sent by the client, then returns that album as a response.
+func getAlbumByID(c *gin.Context) {
+	id := c.Param("id")
+	for _, a := range albums {
+		if a.ID == id {
+			c.IndentedJSON(http.StatusOK, a)
+			return
+		}
+	}
+
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Album not found"})
+}
+
 func main() {
 	router := gin.Default()
 	router.GET("/albums", getAlbums)
+	router.GET("/album/:id", getAlbumByID)
 	router.POST("/albums", postAlbums)
 
 	router.Run("localhost:8111")
