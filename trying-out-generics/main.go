825b732cf9fcfefdf3b270c88bde16d7cc0de1b2
Iaroslav Baslin <wertout77@gmail.com>
2023-04-03 20:29:40 +0300

    feat: generic function using a type constraint via an interface

--- trying-out-generics/main.go
+++ trying-out-generics/main.go
@@ -2,6 +2,12 @@ package main
 
 import "fmt"
 
+// Example of a type constraint as an interface.
+// Common use case is in generics. Like function SumNumbers below. 
+type Number interface {
+    int64 | float64
+}
+
 func main() {
 	ints := map[string]int64{
 		"first":  34,
@@ -21,6 +27,10 @@ func main() {
 	fmt.Printf("Generic Sums: %v and %v\n",
 		SumIntsOrFloats(ints),
 		SumIntsOrFloats(floats))
+
+    fmt.Printf("Generic Sums with Constraint: %v and %v\n",
+    SumNumbers(ints),
+    SumNumbers(floats))
 }
 
 // SumInts adds together the values of m.
@@ -48,4 +58,15 @@ func SumIntsOrFloats[K comparable, V int64 | float64](m map[K]V) V {
 	}
 
 	return sum
+}
+
+
+func SumNumbers[K comparable, V Number](m map[K]V) V {
+    var s V
+    
+    for _, v := range m {
+        s += v
+    }
+
+    return s
 }
